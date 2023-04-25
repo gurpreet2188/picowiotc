49633daadfc221e629d58d586e979915dcbc9d88
gurpreet2188 <74884319+gurpreet2188@users.noreply.github.com>
2023-04-25 11:31:39 +0800

    updated to add prefer networks

--- wifi_creds.py
+++ wifi_creds.py
@@ -1,2 +1,9 @@
-ssid = ''
-password = ''
+"""
+Sample creds dict to store multiple networks with preferred value
+"""
+
+creds = {
+    'ssid1': {'password': '', 'preferred': False},     #network 1
+    'ssid2': {'password': '', 'preferred': False},     #network 2
+    'ssid3': {'password': '', 'preferred': True}       #network 3
+    }
